a1b78fc129d3ac824fea3b156952b2bac084bd23
T-Toshiya <toshiya.tanaka@connehito.com>
2022-09-10 20:26:20 +0900

    add method

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -8,32 +8,56 @@ import (
 
 // HelloHandler /hello のハンドラ
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello, world!\n")
+	if r.Method == http.MethodGet {
+		io.WriteString(w, "Hello, world!\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
 // PostArticleHandler /article のハンドラ
 func PostArticleHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Posting Article...\n")
+	if r.Method == http.MethodPost {
+		io.WriteString(w, "Posting Article...\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
 // ArticleListHandler /article/list のハンドラ
 func ArticleListHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Article List\n")
+	if r.Method == http.MethodGet {
+		io.WriteString(w, "Article List\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
 // ArticleDetailHandler /article/1 のハンドラ
 func ArticleDetailHandler(w http.ResponseWriter, r *http.Request) {
-	articleID := 1
-	resString := fmt.Sprintf("Article No.%d\n", articleID)
-	io.WriteString(w, resString)
+	if r.Method == http.MethodGet {
+		articleID := 1
+		resString := fmt.Sprintf("Article No.%d\n", articleID)
+		io.WriteString(w, resString)
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
 // PostNiceHandler /article/nice のハンドラ
 func PostNiceHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Posting Nice...\n")
+	if r.Method == http.MethodPost {
+		io.WriteString(w, "Posting Nice...\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
 // PostCommentHandler /comment のハンドラ
 func PostCommentHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Posting Comment...\n")
+	if r.Method == http.MethodPost {
+		io.WriteString(w, "Posting Comment...\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
