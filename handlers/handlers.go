dcbd2ee714d66dcfe3069d8add4b2029bff1f7ed
T-Toshiya <toshiya.tanaka@connehito.com>
2022-09-11 12:24:51 +0900

    decode

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,9 +2,11 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"go-api-book/models"
 	"io"
 	"net/http"
+	"strconv"
 )
 
 // HelloHandler /hello のハンドラ
@@ -14,7 +16,26 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 
 // PostArticleHandler /article のハンドラ
 func PostArticleHandler(w http.ResponseWriter, r *http.Request) {
-	article := models.Article1
+	length, err := strconv.Atoi(r.Header.Get("Content-Length"))
+	if err != nil {
+		http.Error(w, "cannot get content length\n", http.StatusBadRequest)
+		return
+	}
+	reqBodybuffer := make([]byte, length)
+
+	if _, err := r.Body.Read(reqBodybuffer); !errors.Is(err, io.EOF) {
+		http.Error(w, "fail to get request body\n", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	var reqArticle models.Article
+	if err := json.Unmarshal(reqBodybuffer, &reqArticle); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+		return
+	}
+
+	article := reqArticle
 	jsonData, err := json.Marshal(article)
 	if err != nil {
 		http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
