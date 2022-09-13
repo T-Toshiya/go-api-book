d05e5879c64a8eb6f7c80b829037f52ababe8d7c
T-Toshiya <toshiya.tanaka@connehito.com>
2022-09-13 23:10:59 +0900

    create article service

--- services/article_service.go
+++ services/article_service.go
@@ -41,3 +41,40 @@ func PostArticleService(article models.Article) (models.Article, error) {
 
 	return newArticle, nil
 }
+
+func ListArticleService(page int) ([]models.Article, error) {
+	db, err := connectDB()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	articles, err := repositories.SelectArticleList(db, page)
+	if err != nil {
+		return nil, err
+	}
+
+	return articles, nil
+}
+
+func PostNiceService(article models.Article) (models.Article, error) {
+	db, err := connectDB()
+	if err != nil {
+		return models.Article{}, err
+	}
+	defer db.Close()
+
+	if err := repositories.UpdateNiceNum(db, article.ID); err != nil {
+		return models.Article{}, err
+	}
+
+	return models.Article{
+		ID:          article.ID,
+		Title:       article.Title,
+		Contents:    article.Contents,
+		UserName:    article.UserName,
+		NiceNum:     article.NiceNum + 1,
+		CommentList: article.CommentList,
+		CreatedAt:   article.CreatedAt,
+	}, nil
+}
