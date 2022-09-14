a96b774030e89818218e052aafc9f5ccd02054f8
T-Toshiya <toshiya.tanaka@connehito.com>
2022-09-14 10:09:27 +0900

    fix handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,8 +3,8 @@ package handlers
 import (
 	"encoding/json"
 	"go-api-book/models"
+	"go-api-book/services"
 	"io"
-	"log"
 	"net/http"
 	"strconv"
 
@@ -19,12 +19,16 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 // PostArticleHandler /article のハンドラ
 func PostArticleHandler(w http.ResponseWriter, r *http.Request) {
 	var reqArticle models.Article
-	if err := json.NewDecoder(r.Body).Decode(reqArticle); err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&reqArticle); err != nil {
 		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
 		return
 	}
 
-	article := reqArticle
+	article, err := services.PostArticleService(reqArticle)
+	if err != nil {
+		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
+		return
+	}
 
 	json.NewEncoder(w).Encode(article)
 }
@@ -45,29 +49,57 @@ func ArticleListHandler(w http.ResponseWriter, r *http.Request) {
 		page = 1
 	}
 
-	log.Println(page)
-
-	articles := []models.Article{models.Article1, models.Article2}
+	articles, err := services.ListArticleService(page)
+	if err != nil {
+		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
+		return
+	}
 	json.NewEncoder(w).Encode(articles)
 }
 
 // ArticleDetailHandler /article/1 のハンドラ
 func ArticleDetailHandler(w http.ResponseWriter, r *http.Request) {
-	articleID := mux.Vars(r)["id"]
-	log.Println(articleID)
+	articleID, err := strconv.Atoi(mux.Vars(r)["id"])
+	if err != nil {
+		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+		return
+	}
 
-	article := models.Article1
+	article, err := services.GetArticleService(articleID)
+	if err != nil {
+		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
+		return
+	}
+
 	json.NewEncoder(w).Encode(article)
 }
 
 // PostNiceHandler /article/nice のハンドラ
 func PostNiceHandler(w http.ResponseWriter, r *http.Request) {
-	article := models.Article1
+	var reqArticle models.Article
+	if err := json.NewDecoder(r.Body).Decode(&reqArticle); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+		return
+	}
+	article, err := services.PostNiceService(reqArticle)
+	if err != nil {
+		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
+		return
+	}
 	json.NewEncoder(w).Encode(article)
 }
 
 // PostCommentHandler /comment のハンドラ
 func PostCommentHandler(w http.ResponseWriter, r *http.Request) {
-	comment := models.Comment1
+	var reqComment models.Comment
+	if err := json.NewDecoder(r.Body).Decode(&reqComment); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+	}
+
+	comment, err := services.PostCommentService(reqComment)
+	if err != nil {
+		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
+		return
+	}
 	json.NewEncoder(w).Encode(comment)
 }
